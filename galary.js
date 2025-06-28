9456ec77f85a95eab2136e6eb909a6dcc93ff019
Andrey <andreyborisov08@yandex.ru>
2025-06-28 07:51:20 +0300

    imafes round 2

--- galary.js
+++ galary.js
@@ -1,4 +1,4 @@
-for (let i = 1; i < 40; i++) {
+for (let i = 1; i <= 63; i++) {
     document.querySelector('.swiper-wrapper.swiper__wrapper').innerHTML += `
         <div class="swiper-slide slider__item">
             <div class="slider_img" data-swiper-parallax="20%" style="background-image: url(./images/${i}.jpg);"></div>
@@ -6,21 +6,13 @@ for (let i = 1; i < 40; i++) {
 }
 
 const cliderMain = new Swiper('.slider_main', {
-    freeMode: {
-        enabled: true,
-        sticky: false, // Отключает прилипание слайдов
-        momentumRatio: false, // Уменьшает инерцию, делает движение более контролируемым
-        momentumBounce: false // Отключает эффект отскока
-    },
+    freeMode: true,
     loop: true,
     autoplay: {
         delay: 3000,
         disableOnInteraction: false
     },
     
-    mousewheel: {
-        sensitivity: 10,
-    },
     parallax: true,
     lazy: true,
     breakpoints: {
